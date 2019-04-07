5407b003465701c00381e9499e2f32024f1e0907
Sandrine Ayral <sandrine.ayral@gmail.com>
2019-04-07 16:06:04 -0400

    Add route for import feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'jobs#index'
-  resources :jobs, only: :index
+  resources :jobs, only: :index do
+    collection do
+      post :import
+    end
+  end
 end
